0fef14fccdeecee5d70731a890c20b40dd5dd715
JorgeLaptopGitKraken <fjorge8@gmail.com>
2022-12-18 16:13:48 +0100

    updateCurrentConfig

--- src/components/color/CurrentConfig.vue
+++ src/components/color/CurrentConfig.vue
@@ -16,9 +16,17 @@ const handeOnButtonSettings = () => {
   <div
     class="sm:absolute top-10 w-full sm:w-[20rem] left-10 mt-8 top-0 pa-2 grid grid-cols-1 pa-5"
   >
-    <div class="mb-5 pa-0 text-left">
-      <ColorLibrary class="my-2" />
-      <v-divider></v-divider>
+    <div
+      class="mb-1 pa-0 rounded-md border-2 drop-shadow-lg opacity-100 text-black pa-5"
+    >
+      <h2 class="uppercase font-bold text-xl">Quick Selection</h2>
+      <div class="pa-0">
+        <h3 class="mb-1 text-grey text-lg">Colors</h3>
+        <div class="mb-5 pa-0 text-left">
+          <ColorLibrary class="my-2" />
+          <v-divider></v-divider>
+        </div>
+      </div>
     </div>
     <div
       class="mb-1 pa-0 rounded-md border-2 drop-shadow-lg opacity-100 text-black pa-5"
