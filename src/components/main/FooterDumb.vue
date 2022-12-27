1da7f304174b62d974a1a3c2cbcfe13ed3bc69f8
JorgeLaptopGitKraken <fjorge8@gmail.com>
2022-12-27 17:55:55 +0100

    update: presets

--- src/components/main/FooterDumb.vue
+++ src/components/main/FooterDumb.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="box-shadow-2 h-[40px] border-t-2 py-3 px-12 text-right">
-    PixelArt Tool v0.1
+    PixelArt Tool v0.2 Presets -
     <v-icon icon="mdi-copyright" size="15" class="mb-1" />
     Jorge Flores -
     {{ new Date().getFullYear() }}
